a3820ed9e8fd617673e1e02d1defc1914a952118
Jens Schulze <jens.schulze@commercetools.com>
2022-07-07 12:12:54 +0200

    feat(ctpclient): api root instantiation with middlewares

--- packages/platform-sdk/src/ctp/ctp-client.ts
+++ packages/platform-sdk/src/ctp/ctp-client.ts
@@ -1,22 +1,29 @@
-import {ApiRoot, ClientResponse, Middleware} from './../generated/index'
+import { ApiRoot, ClientResponse, Middleware } from './../generated/index'
+import { Client } from '@commercetools/sdk-client-v2'
 
 export function createApiBuilderFromCtpClient(
-  ctpClient: any,
-  baseUri?: string
+  ctpClient: Client | any,
+  baseUri?: string,
+  middlewares?: Middleware[]
 ): ApiRoot {
   return new ApiRoot({
     executeRequest: ctpClient.execute,
     baseUri: baseUri,
+    middlewares: middlewares,
   })
 }
 
-export function createConcurrentModificationMiddleware(maxRetries: number = 3): Middleware
-{
+export function createConcurrentModificationMiddleware(
+  maxRetries: number = 3
+): Middleware {
   return (request, executor) => {
     let retryCount = 0
     function retry(response: ClientResponse): Promise<ClientResponse> {
       if (response.statusCode == 409 && retryCount < maxRetries) {
-        request.body = {...request.body, version: response.body.errors[0].currentVersion} // modify version
+        request.body = {
+          ...request.body,
+          version: response.body.errors[0].currentVersion,
+        } // modify version
         retryCount++
         return executor(request).then(retry)
       }
